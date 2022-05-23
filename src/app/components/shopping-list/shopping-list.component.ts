8dd8eb98803f26ce1261cbf2c1f07127d0f6891d
MJWORKS <mjworks2020@outlook.com>
2022-05-22 22:55:25 -0300

    Added new changes

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
-  constructor() { }
+  public ingredients: Ingredient[] = [
+    new Ingredient('Maças', 5),
+    new Ingredient('Laranjas', 7),
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
