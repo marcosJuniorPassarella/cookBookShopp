7596a7590ad3526c9ba84d26ca80f8ae5d53e727
MJWORKS <mjworks2020@outlook.com>
2022-07-10 18:50:35 -0300

    Added routerlink navigation in the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +6,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
   collapsed = true;
   constructor() {}
 
   ngOnInit(): void {}
-
-  onSelect(feature: string): void {
-    this.featureSelected.emit(feature);
-  }
 }
