152f1060c4ec10ae7e30c8ac21fb297cf1601b50
MJWORKS <mjworks2020@outlook.com>
2022-07-11 20:14:29 -0300

    Added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { RecipesStartComponent } from './components/recipes/recipes-start/recipes-start.component';
+import { RecipesDetailComponent } from './components/recipes/recipes-detail/recipes-detail.component';
 
 const appRoutes: Routes = [
   {
@@ -12,6 +14,16 @@ const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
+    children: [
+      {
+        path: '',
+        component: RecipesStartComponent,
+      },
+      {
+        path: ':id',
+        component: RecipesDetailComponent,
+      },
+    ],
   },
   {
     path: 'shopping-list',
