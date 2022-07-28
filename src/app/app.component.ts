fbdb71ed06b4cde468a6cf1aa13742bcd435712a
MJWORKS <mjworks2020@outlook.com>
2022-07-28 09:59:06 -0300

    Cleaned the app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string): void {
-    this.loadedFeature = feature;
-  }
-}
+export class AppComponent {}
