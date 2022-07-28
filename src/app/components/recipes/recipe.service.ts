47d963eeb424dc2a7f1e63686fd137cbeb1e7ea2
MJWORKS <mjworks2020@outlook.com>
2022-07-28 10:11:11 -0300

    Cleaned the app

--- src/app/components/recipes/recipe.service.ts
+++ src/app/components/recipes/recipe.service.ts
@@ -1,10 +1,10 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredients.model';
 import { Recipe } from './recipes.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
       'A test recipe',
--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -1,22 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipes.model';
-import { RecipeService } from './recipe.service';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
 })
-export class RecipesComponent implements OnInit {
-  selectedRecipe!: Recipe;
-
-  constructor(private recipeService: RecipeService) {}
-
-  ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe({
-      next: (res: Recipe) => {
-        this.selectedRecipe = res;
-      },
-    });
-  }
-}
+export class RecipesComponent {}
