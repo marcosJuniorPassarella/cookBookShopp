70022c0ab60e6c193d77f83c25889746b27d6f25
MJWORKS <mjworks2020@outlook.com>
2022-07-28 09:50:35 -0300

    Added functionality navigate to new recipe page

--- src/app/components/recipes/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipes.model';
 
@@ -11,7 +12,11 @@ export class RecipesListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   public recipes!: Recipe[];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
@@ -19,5 +24,9 @@ export class RecipesListComponent implements OnInit {
 
   onRecipeSelected(recipe: Recipe): void {
     this.recipeWasSelected.emit(recipe);
+  }
+
+  onNewRecipe(): void {
+    void this.router.navigate(['new'], { relativeTo: this.route });
   }
 }
