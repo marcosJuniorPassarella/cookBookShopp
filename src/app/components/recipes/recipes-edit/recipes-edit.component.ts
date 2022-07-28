81cc2eb8adaa5b0a2598d55aaacfd1f425272fc5
MJWORKS <mjworks2020@outlook.com>
2022-07-28 09:47:09 -0300

    Added new changes in the RecipesEdit Component

--- src/app/components/recipes/recipes-edit/recipes-edit.component.ts
+++ src/app/components/recipes/recipes-edit/recipes-edit.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-edit',
   templateUrl: './recipes-edit.component.html',
-  styleUrls: ['./recipes-edit.component.scss']
+  styleUrls: ['./recipes-edit.component.scss'],
 })
 export class RecipesEditComponent implements OnInit {
+  id!: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe({
+      next: (params: Params) => {
+        this.id = +params['id'];
+        this.editMode = params['id'] != null;
+      },
+    });
   }
-
 }
