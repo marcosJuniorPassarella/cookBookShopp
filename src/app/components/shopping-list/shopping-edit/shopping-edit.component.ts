85a9797a4ef80b958f37e3a2c071c0645cf2c1e8
MJWORKS <mjworks2020@outlook.com>
2022-07-29 16:07:46 -0300

    Added update existing item method

--- src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -35,7 +35,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm): void {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shopplService.addIngredient(newIngredient);
+    this.editMode
+      ? this.shopplService.updateIngredient(this.editedItemIndex, newIngredient)
+      : this.shopplService.addIngredient(newIngredient);
   }
 
   ngOnDestroy(): void {
