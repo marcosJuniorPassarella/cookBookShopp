a214392001cf99440f89d6d3eb76961af73225ac
MJWORKS <mjworks2020@outlook.com>
2022-08-04 15:54:32 -0300

    Added get recipes subject method

--- src/app/components/recipes/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  OnDestroy,
+} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipes.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss'],
 })
-export class RecipesListComponent implements OnInit {
+export class RecipesListComponent implements OnInit, OnDestroy {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  private recipeSubscription!: Subscription;
   public recipes!: Recipe[];
 
   constructor(
@@ -19,6 +27,13 @@ export class RecipesListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.getRecipes();
+  }
+
+  getRecipes(): void {
+    this.recipeSubscription = this.recipeService.recipesChanged.subscribe({
+      next: (recipe: Recipe[]) => (this.recipes = recipe),
+    });
     this.recipes = this.recipeService.getRecipes();
   }
 
@@ -28,5 +43,9 @@ export class RecipesListComponent implements OnInit {
 
   onNewRecipe(): void {
     void this.router.navigate(['new'], { relativeTo: this.route });
+  }
+
+  ngOnDestroy(): void {
+    this.recipeSubscription && this.recipeSubscription.unsubscribe();
   }
 }
