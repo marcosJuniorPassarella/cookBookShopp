7962f163da715985496fdfb456dddb6d6c039a1f
MJWORKS <mjworks2020@outlook.com>
2022-08-04 16:27:31 -0300

    Added delete method in the recipe service

--- src/app/components/recipes/recipe.service.ts
+++ src/app/components/recipes/recipe.service.ts
@@ -46,4 +46,9 @@ export class RecipeService {
     this.recipes[index] = newRecipe;
     this.recipesChanged.next(this.recipes.slice());
   }
+
+  deleteRecipes(index: number): void {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
