e94c3bce0cc9f7e40fb4c162beff89299e8efd61
MJWORKS <mjworks2020@outlook.com>
2022-08-05 09:17:28 -0300

    Created data storage service and added url

--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -1,9 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { RecipeService } from '../../components/recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes(): void {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(environment.firebaseUrl, recipes).subscribe({
+      next: (res) => console.log(res),
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseUrl: 'https://recipe-book-course-a2e6d-default-rtdb.firebaseio.com/recipes.json',
 };
 
 /*
