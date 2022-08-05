b5d1591d40e461d586dba6546a13e0195a61277e
MJWORKS <mjworks2020@outlook.com>
2022-08-05 09:23:06 -0300

    Added onSaveData method in the header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../../shared/services/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  constructor() {}
+  constructor(private dataStorageService: DataStorageService) {}
 
   ngOnInit(): void {}
+
+  onSaveData(): void {
+    this.dataStorageService.storeRecipes();
+  }
 }
