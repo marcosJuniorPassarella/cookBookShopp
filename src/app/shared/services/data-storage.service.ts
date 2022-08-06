d7ff4ee59621bde2382f58e4bf38e3363cf3a595
MJWORKS <mjworks2020@outlook.com>
2022-08-06 15:34:50 -0300

    Implemented get auth token

--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, tap, Observable } from 'rxjs';
+import { map, tap, Observable, take, exhaustMap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { RecipeService } from '../../pages/recipes/recipe.service';
 import { Recipe } from '../../pages/recipes/recipes.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,11 @@ import { Recipe } from '../../pages/recipes/recipes.model';
 export class DataStorageService {
   url = environment.firebaseUrl;
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes(): void {
     const recipes = this.recipeService.getRecipes();
@@ -22,7 +27,16 @@ export class DataStorageService {
   }
 
   fetchRecipes(): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>(this.url).pipe(
+    return this.authService.user.pipe(
+      // take(1) é um operador que permite que o observable só seja executado uma vez e em seguida seja descartado automaticamente
+      take(1),
+      // exhaustMap é um operador que permite que o observable seja executado enquanto outro observable é executado
+      // ou seja, enquanto o primeiro for executado, o segundo não será executado e o resultado será retornado
+      exhaustMap((user) => {
+        return this.http.get<Recipe[]>(this.url, {
+          params: new HttpParams().set('auth', user.token),
+        });
+      }),
       map((recipes) => {
         return recipes.map((recipe) => {
           return {
