ce90659dc0c8dae31f3d0a94800a557f01b2304f
MJWORKS <mjworks2020@outlook.com>
2022-08-11 21:22:19 -0300

    Added the auth guard

---
+++ src/app/shared/auth/auth-guard.ts
@@ -0,0 +1,28 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.authService.user.pipe(
+      map((user) => {
+        // se tiver valor retornará true senão retornará false
+        return !!user;
+      })
+    );
+  }
+}
