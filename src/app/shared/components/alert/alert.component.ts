135444eb992e664b41da4dfd63dae90e2f8be0e1
MJWORKS <mjworks2020@outlook.com>
2022-08-12 21:30:35 -0300

    Created the dinamic modal

---
+++ src/app/shared/components/alert/alert.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss'],
+})
+export class AlertComponent {
+  @Input()
+  message!: string;
+}
