cfdea80ca1a8d20920231e493e2deec4c48fc23e
MJWORKS <mjworks2020@outlook.com>
2022-08-16 18:38:16 -0300

    Created the recipes module

---
+++ src/app/pages/recipes/recipes.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecipesDetailComponent } from './recipes-detail/recipes-detail.component';
+import { RecipesEditComponent } from './recipes-edit/recipes-edit.component';
+import { RecipesItemComponent } from './recipes-list/recipes-item/recipes-item.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipesStartComponent } from './recipes-start/recipes-start.component';
+import { RecipesComponent } from './recipes.component';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipesItemComponent,
+    RecipesStartComponent,
+    RecipesEditComponent,
+  ],
+  imports: [CommonModule],
+})
+export class RecipesModule {}
