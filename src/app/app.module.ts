6d234b4fb68adf5ff148def97c6537e0d589494b
MJWORKS <mjworks2020@outlook.com>
2022-08-17 16:18:14 -0300

    Add StoreModule config for the shoppingListReducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
 import { HeaderComponent } from './pages/header/header.component';
 import { AuthModule } from './auth/auth.module';
+import { shoppingListReducer } from './pages/shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -16,6 +19,7 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot({ shoppingListR: shoppingListReducer }),
     SharedModule,
     AuthModule,
     CoreModule,
