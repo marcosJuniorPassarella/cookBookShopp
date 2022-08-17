9438b0373ba9230af19609940a4458b64bb681e0
MJWORKS <mjworks2020@outlook.com>
2022-08-17 16:03:27 -0300

    Created the shopping list actions file

--- src/app/pages/shopping-list/store/shopping-list.reducer.ts
+++ src/app/pages/shopping-list/store/shopping-list.reducer.ts
@@ -1,5 +1,6 @@
 import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/models/ingredients.model';
+import { ADD_INGREDIENT } from './shopping-list.actions';
 
 const initialState = {
   ingredients: [new Ingredient('Maças', 5), new Ingredient('Laranjas', 7)],
@@ -7,7 +8,7 @@ const initialState = {
 
 export function shoppingListReducer(state = initialState, action: Action) {
   switch (action.type) {
-    case 'ADD_INGREDIENT':
+    case ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action],
