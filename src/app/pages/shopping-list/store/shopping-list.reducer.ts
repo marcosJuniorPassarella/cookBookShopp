17050e793a7a495338537fa5dc81c44900e0482e
MJWORKS <mjworks2020@outlook.com>
2022-08-17 16:11:41 -0300

    Added new updates in the shopping list reducer

--- src/app/pages/shopping-list/store/shopping-list.reducer.ts
+++ src/app/pages/shopping-list/store/shopping-list.reducer.ts
@@ -1,17 +1,19 @@
-import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/models/ingredients.model';
-import { ADD_INGREDIENT } from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
   ingredients: [new Ingredient('Maças', 5), new Ingredient('Laranjas', 7)],
 };
 
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.AddIngredient
+) {
   switch (action.type) {
-    case ADD_INGREDIENT:
+    case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients, action],
+        ingredients: [...state.ingredients, action.payload],
       };
   }
 }
