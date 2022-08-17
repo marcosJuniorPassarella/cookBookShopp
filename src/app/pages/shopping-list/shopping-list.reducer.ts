91ac9391e2a610cd988297df0dade0e3965071b0
MJWORKS <mjworks2020@outlook.com>
2022-08-17 15:02:11 -0300

    Added initial shopping list reducer implementation

--- src/app/pages/shopping-list/shopping-list.reducer.ts
+++ src/app/pages/shopping-list/shopping-list.reducer.ts
@@ -1,7 +1,16 @@
+import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/models/ingredients.model';
 
 const initialState = {
   ingredients: [new Ingredient('Maças', 5), new Ingredient('Laranjas', 7)],
 };
 
-export function shoppingListReducer(state = initialState, action) {}
+export function shoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action],
+      };
+  }
+}
