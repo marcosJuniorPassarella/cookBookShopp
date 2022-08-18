093413d9bd672e52552b419fddcd06c13d1a86fc
MJWORKS <mjworks2020@outlook.com>
2022-08-18 10:36:22 -0300

    Added update and delete const identificators

--- src/app/pages/shopping-list/store/shopping-list.actions.ts
+++ src/app/pages/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from '../../../shared/models/ingredients.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export type ShoppingListActions = AddIngredient | AddIngredients;
 export class AddIngredient implements Action {
