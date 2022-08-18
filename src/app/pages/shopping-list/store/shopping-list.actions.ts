d167958047d949bee5b1433f2bb2c95113794822
MJWORKS <mjworks2020@outlook.com>
2022-08-18 10:02:48 -0300

    Created shoppingListActions type and implemented

--- src/app/pages/shopping-list/store/shopping-list.actions.ts
+++ src/app/pages/shopping-list/store/shopping-list.actions.ts
@@ -4,6 +4,7 @@ import { Ingredient } from '../../../shared/models/ingredients.model';
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 
+export type ShoppingListActions = AddIngredient | AddIngredients;
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
 
