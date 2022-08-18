c90149604a8e2beab5487225cd3654833c0c47ac
MJWORKS <mjworks2020@outlook.com>
2022-08-18 09:34:13 -0300

    Added type and class for AddIngredients action

--- src/app/pages/shopping-list/store/shopping-list.actions.ts
+++ src/app/pages/shopping-list/store/shopping-list.actions.ts
@@ -1,9 +1,17 @@
 import { Action } from '@ngrx/store';
 import { Ingredient } from '../../../shared/models/ingredients.model';
+
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
 
   constructor(public payload: Ingredient) {}
+}
+
+export class AddIngredients implements Action {
+  readonly type = ADD_INGREDIENTS;
+
+  constructor(public payload: Ingredient[]) {}
 }
