2ebdfdc287c2113db1ba1411c73800bff86d99fe
MJWORKS <mjworks2020@outlook.com>
2022-08-18 10:38:52 -0300

    Added updateIngredient class in action file

--- src/app/pages/shopping-list/store/shopping-list.actions.ts
+++ src/app/pages/shopping-list/store/shopping-list.actions.ts
@@ -7,6 +7,7 @@ export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
 export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export type ShoppingListActions = AddIngredient | AddIngredients;
+
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
 
@@ -17,4 +18,10 @@ export class AddIngredients implements Action {
   readonly type = ADD_INGREDIENTS;
 
   constructor(public payload: Ingredient[]) {}
+}
+
+export class UpdateIngredient implements Action {
+  readonly type = UPDATE_INGREDIENT;
+
+  constructor(public payload: { index: number; ingredient: Ingredient }) {}
 }
