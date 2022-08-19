3cce5d5a0cd70c2dd20ebc5edc97c0b927478d3d
MJWORKS <mjworks2020@outlook.com>
2022-08-19 09:11:27 -0300

    Added the StartEdit class and your string type

--- src/app/pages/shopping-list/store/shopping-list.actions.ts
+++ src/app/pages/shopping-list/store/shopping-list.actions.ts
@@ -5,6 +5,8 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
 export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export const START_EDIT = 'START_EDIT';
+export const STOP_EDIT = 'STOP_EDIT';
 
 export type ShoppingListActions =
   | AddIngredient
@@ -32,6 +34,12 @@ export class UpdateIngredient implements Action {
 
 export class DeleteIngredient implements Action {
   readonly type = DELETE_INGREDIENT;
+
+  constructor(public payload: number) {}
+}
+
+export class StartEdit implements Action {
+  readonly type = START_EDIT;
 
   constructor(public payload: number) {}
 }
