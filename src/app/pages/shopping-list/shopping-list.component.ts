ecde42cb15bf172c8d6ae4655c70ec101f26923d
MJWORKS <mjworks2020@outlook.com>
2022-08-24 19:44:27 -0300

    Removed the shopping list service

--- src/app/pages/shopping-list/shopping-list.component.ts
+++ src/app/pages/shopping-list/shopping-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { ShoppingListService } from './shopping-list.service';
 import { Ingredient } from 'src/app/shared/models/ingredients.model';
 import * as fromShoppingList from './store/shopping-list.reducer';
 import * as ShoppingListActions from './store/shopping-list.actions';
